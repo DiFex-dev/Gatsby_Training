a69830cc6c2cc34c33972bec6a0a594dcc62a99f
DiFex-dev <derenok2002@gmail.com>
2022-02-15 16:50:16 +0300

    fix№2 basic container

--- src/templates/HomePageTemplate/HomePageTemplate.jsx
+++ src/templates/HomePageTemplate/HomePageTemplate.jsx
@@ -7,13 +7,13 @@ import styles from './HomePageTemplate.styles';
 
 const HomePageTemplate = ({ pageContext: { PageData } }) => {
   const [showModal, setShowModal] = useState(true);
-  const [showContent, setShowContent] = useState(true);
+  const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
     setTimeout(() => setShowModal(false), 3000);
   }, []);
   useEffect(() => {
-    setShowContent(showModal);
+    setShowContent(!showModal);
   }, [showModal]);
   return (
     <Layout title={PageData.seo.title} description={PageData.seo.description} image={PageData.seo.image}>
@@ -24,7 +24,7 @@ const HomePageTemplate = ({ pageContext: { PageData } }) => {
               <Welcome />
             </div>
           )}
-          <div className={`container-todo ${!showContent ? 'active' : ''}`}></div>
+          <div className={`container-todo ${showContent ? 'active' : ''}`}></div>
         </Wrapper>
       </div>
     </Layout>
